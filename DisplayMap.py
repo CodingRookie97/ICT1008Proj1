71012b93a26441644b504b6c680b48c590ccd1e0
Jerone Poh <pohjerone97@gmail.com>
2020-02-12 22:50:38 +0800

    hi

--- DisplayMap.py
+++ DisplayMap.py
@@ -5,4 +5,5 @@ nodeMap = folium.Map(location=[1.4053, 103.9021], zoom_start=16)
 nodes = os.path.join('nodes.json')
 folium.GeoJson(nodes).add_to(nodeMap)
 
-nodeMap.save('C:\\Users\\Jerone Poh\\Desktop\\punggol.html')
+# The file directory will be different for each computer, just put your files into your own desktop directory
+# nodeMap.save('C:\\Users\\Jerone Poh\\Desktop\\punggol.html')
